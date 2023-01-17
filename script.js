8759ad4cc1f87551cdee88b676fbeb3cc3de0a5b
Michalous <49964028+Michalous@users.noreply.github.com>
2023-01-17 21:12:04 +0000

    time classes added and change according to time

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var now = moment().format("dddd, MMMM Do")
+var nowHour = moment().format('H')
+
+console.log(nowHour)
 
 $(document).ready(function() {
     $('#currentDay').text(now)
     for (var i = 0; i <= 8; i++) {
+        var toInsert = (i < 3) ? `${i + 9} AM` : ((i == 3) ? "12 PM" : `${i - 3} PM`)
+        var classInsert = ((i + 9) > nowHour) ? 'future' : ((i + 9) == nowHour ? 'present' : 'past')
         $('.container').append(`<div class="row">
-                                    <div class="hour">${i + 9}</div>
-                                    <div class="description future">
+                                    <div class="hour">${toInsert}</div>
+                                    <div class="description ${classInsert}">
                                         <textarea>Hello, World!</textarea>
                                     </div>
                                     <div class="saveBtn"><i class="fa fa-solid fa-lock"></i></div>
