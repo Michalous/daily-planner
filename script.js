52e5a7a5517dcff701cbcb10a4290adb0d97c1b1
Michalous <49964028+Michalous@users.noreply.github.com>
2023-01-17 20:36:10 +0000

    front end added

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ var now = moment().format("dddd, MMMM Do")
 $(document).ready(function() {
     $('#currentDay').text(now)
     for (var i = 0; i <= 8; i++) {
-        $('.container').append(`<div class="hour">${i + 9}</div>`)
+        $('.container').append(`<div class="row">
+                                    <div class="hour">${i + 9}</div>
+                                    <div class="description future">
+                                        <textarea>Hello, World!</textarea>
+                                    </div>
+                                    <div class="saveBtn"><i class="fa fa-solid fa-lock"></i></div>
+                                </div>`)
     }
 })
